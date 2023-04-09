833a6bda9beb62b5ab71a9a987ea2a41e2ffe7c6
Mohamed Mokhtar <Mohamed_201900731@fci.helwan.edu.eg>
2023-04-09 06:24:12 +0200

    a

--- src/Components/util/FooterBar.js
+++ src/Components/util/FooterBar.js
@@ -2,7 +2,10 @@ import React from "react";
 
 function FooterBar() {
   return (
-    <div className="bg-main text-white p-4 text-center">
+    <div
+      className="bg-main text-white p-4 text-center"
+      style={{ zIndex: "100", position: "sticky" }}
+    >
       All copyright reserved &copy;
     </div>
   );
