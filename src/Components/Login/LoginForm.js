eaec043758cfd3ca3adf48fe6716ab25aa121a59
MohamedMokhtar6 <mohamed01017715716@gmail.com>
2023-06-18 19:15:00 +0300

    a

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Col, Container, Row, Spinner } from "react-bootstrap";
-import { Bounce } from "react-reveal";
 import { Link } from "react-router-dom";
 import LoginHook from "../../hook/login-hook";
 import { ToastContainer } from "react-toastify";
@@ -17,73 +16,71 @@ function LoginForm() {
   ] = LoginHook();
   return (
     <Container style={{ minHeight: "82vh" }}>
-      <Bounce left>
-        <Row className=" m-5 justify-content-center text-white ">
-          <Col
-            style={{ width: "400px", height: "600px", flex: "none" }}
-            className=" p-0 reduceL d-flex flex-column text-center align-items-center  bg-main  "
+      <Row className=" m-5 justify-content-center text-white ">
+        <Col
+          style={{ width: "400px", height: "600px", flex: "none" }}
+          className=" p-0 reduceL d-flex flex-column text-center align-items-center  bg-main  "
+        >
+          <h2 className="fw-bold" style={{ marginTop: "150px" }}>
+            Sign In
+          </h2>
+          <input
+            value={email}
+            onChange={onChangeEmail}
+            type={"email"}
+            placeholder="Email"
+            className="my-3 inp"
+          />
+          <input
+            value={password}
+            onChange={onChangePassword}
+            type={"password"}
+            placeholder="Password"
+            className="my-3 inp"
+          />
+          <Link className="link  mt-4 " to={"/forgotpassword"}>
+            Forgot your password?
+          </Link>
+          <button
+            onClick={onSubmit}
+            className="text-main my-3 bt2  my-4"
+            style={{ backgroundColor: "var(--thrdColor)" }}
           >
-            <h2 className="fw-bold" style={{ marginTop: "150px" }}>
-              Sign In
-            </h2>
-            <input
-              value={email}
-              onChange={onChangeEmail}
-              type={"email"}
-              placeholder="Email"
-              className="my-3 inp"
-            />
-            <input
-              value={password}
-              onChange={onChangePassword}
-              type={"password"}
-              placeholder="Password"
-              className="my-3 inp"
-            />
-            <Link className="link  mt-4 " to={"/forgotpassword"}>
-              Forgot your password?
-            </Link>
+            Sign IN
+          </button>
+          {isPress === true ? (
+            loading === true ? (
+              <Spinner
+                animation="border"
+                role="status"
+                variant="danger"
+              ></Spinner>
+            ) : null
+          ) : null}
+        </Col>
+        <Col
+          style={{
+            width: "400px",
+            height: "600px",
+            flex: "none",
+            backgroundColor: "var(--thrdColor)",
+          }}
+          className=" p-0 reduceR  d-flex flex-column align-items-center justify-content-center "
+        >
+          <h2 className="my-2 text-main">Sign Up</h2>
+          <p className="my-2 text-main">
+            Sign up here if you don't have account.
+          </p>
+          <Link to={"/signup"} className="link">
             <button
-              onClick={onSubmit}
-              className="text-main my-3 bt2  my-4"
-              style={{ backgroundColor: "var(--thrdColor)" }}
+              className="text-white my-3 bt2  my-4"
+              style={{ backgroundColor: "var(--mainColor)" }}
             >
-              Sign IN
+              Sign Up Now
             </button>
-            {isPress === true ? (
-              loading === true ? (
-                <Spinner
-                  animation="border"
-                  role="status"
-                  variant="danger"
-                ></Spinner>
-              ) : null
-            ) : null}
-          </Col>
-          <Col
-            style={{
-              width: "400px",
-              height: "600px",
-              flex: "none",
-              backgroundColor: "var(--thrdColor)",
-            }}
-            className=" p-0 reduceR  d-flex flex-column align-items-center justify-content-center "
-          >
-            <h2 className="my-2 text-main">Sign Up</h2>
-            <p className="my-2 text-main">
-              Sign up here if you don't have account.
-            </p>
-            <Link to={"/signup"} className="link">
-              <button
-                className="text-white my-3 bt2  my-4"
-                style={{ backgroundColor: "var(--mainColor)" }}
-              >
-                Sign Up Now
-              </button>
-            </Link>
-          </Col>
-        </Row>
-      </Bounce>
+          </Link>
+        </Col>
+      </Row>
       <ToastContainer />
     </Container>
   );
--- src/Components/Login/SignUpForm.js
+++ src/Components/Login/SignUpForm.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { Col, Container, Row, Spinner } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import { Bounce } from "react-reveal";
 import SignUpFormHook from "../../hook/signup-form-hook";
 import { ToastContainer } from "react-toastify";
 function SignUpForm() {
@@ -22,81 +21,79 @@ function SignUpForm() {
   ] = SignUpFormHook();
   return (
     <Container style={{ minHeight: "82vh" }}>
-      <Bounce right>
-        <Row className=" m-5 justify-content-center ">
-          <Col
-            style={{ width: "400px", height: "600px", flex: "none" }}
-            className="bg-thr p-0 reduceL   d-flex flex-column align-items-center justify-content-center "
-          >
-            <h2 className="my-2 text-main">Sign In</h2>
-            <p className="my-2 text-main">Sign in here if you have account.</p>
-            <Link to={"/login"} className="link">
-              <button className="text-white border-main bt my-3 bg-main">
-                SING IN
-              </button>
-            </Link>
-          </Col>
-          <Col
-            style={{ width: "400px", height: "600px", flex: "none" }}
-            className=" p-0 reduceR d-flex flex-column text-center align-items-center justify-content-center bg-main  "
-          >
-            <h2 className="fw-bold text-white" style={{ marginTop: "50px" }}>
-              Sign Up
-            </h2>
-            <input
-              value={name}
-              onChange={onChangeName}
-              type={"text"}
-              placeholder="Your Name"
-              className="my-3 inp"
-            />
-            <input
-              value={email}
-              onChange={onChangeEmail}
-              type={"email"}
-              placeholder="Email"
-              className="my-3 inp"
-            />
-            <input
-              value={phone}
-              onChange={onChangePhone}
-              type={"phone"}
-              placeholder="Phone"
-              className="my-3 inp"
-            />
-            <input
-              value={password}
-              onChange={onChangePassword}
-              type={"password"}
-              placeholder="Password"
-              className="my-3 inp"
-            />
-            <input
-              value={confirmPassword}
-              onChange={onChangeConfirmPassword}
-              type={"password"}
-              placeholder="confirm Password"
-              className="my-3 inp"
-            />
-            <button
-              onClick={OnSubmit}
-              className="text-main my-3 bt2  my-4"
-              style={{ backgroundColor: "var(--thrdColor)" }}
-            >
-              SING UP
+      <Row className=" m-5 justify-content-center ">
+        <Col
+          style={{ width: "400px", height: "600px", flex: "none" }}
+          className="bg-thr p-0 reduceL   d-flex flex-column align-items-center justify-content-center "
+        >
+          <h2 className="my-2 text-main">Sign In</h2>
+          <p className="my-2 text-main">Sign in here if you have account.</p>
+          <Link to={"/login"} className="link">
+            <button className="text-white border-main bt my-3 bg-main">
+              SING IN
             </button>
-            {isPress === true ? (
-              loading === true ? (
-                <Spinner
-                  animation="border"
-                  role="status "
-                  variant="danger"
-                ></Spinner>
-              ) : null
-            ) : null}
-          </Col>
-        </Row>
-      </Bounce>
+          </Link>
+        </Col>
+        <Col
+          style={{ width: "400px", height: "600px", flex: "none" }}
+          className=" p-0 reduceR d-flex flex-column text-center align-items-center justify-content-center bg-main  "
+        >
+          <h2 className="fw-bold text-white" style={{ marginTop: "50px" }}>
+            Sign Up
+          </h2>
+          <input
+            value={name}
+            onChange={onChangeName}
+            type={"text"}
+            placeholder="Your Name"
+            className="my-3 inp"
+          />
+          <input
+            value={email}
+            onChange={onChangeEmail}
+            type={"email"}
+            placeholder="Email"
+            className="my-3 inp"
+          />
+          <input
+            value={phone}
+            onChange={onChangePhone}
+            type={"phone"}
+            placeholder="Phone"
+            className="my-3 inp"
+          />
+          <input
+            value={password}
+            onChange={onChangePassword}
+            type={"password"}
+            placeholder="Password"
+            className="my-3 inp"
+          />
+          <input
+            value={confirmPassword}
+            onChange={onChangeConfirmPassword}
+            type={"password"}
+            placeholder="confirm Password"
+            className="my-3 inp"
+          />
+          <button
+            onClick={OnSubmit}
+            className="text-main my-3 bt2  my-4"
+            style={{ backgroundColor: "var(--thrdColor)" }}
+          >
+            SING UP
+          </button>
+          {isPress === true ? (
+            loading === true ? (
+              <Spinner
+                animation="border"
+                role="status "
+                variant="danger"
+              ></Spinner>
+            ) : null
+          ) : null}
+        </Col>
+      </Row>
       <ToastContainer />
     </Container>
   );
